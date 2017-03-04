d30366230e1690139a611834e0012da38f9928cf
Amanda Morris <amandafmorris@gmail.com>
2017-03-04 23:01:47 +0000

    More recursive functions added

--- recursion.py
+++ recursion.py
@@ -44,7 +44,13 @@ def print_all_tree_data(tree):
         3
 
     """
-    pass
+    print tree.data
+    if tree.children == []:
+        return
+
+    for child in tree.children:
+        print_all_tree_data(child)
+
 
 # 3. Write a function that uses recursion to find the length of a list.
 
